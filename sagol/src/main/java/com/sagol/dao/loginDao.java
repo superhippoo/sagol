897fc13a4005a5f15560fb968fb4e494217bc60f
superhippoo <71534340+superhippoo@users.noreply.github.com>
2021-08-29 13:21:17 +0900

    Delete sagol directory

--- sagol/src/main/java/com/sagol/dao/loginDao.java
+++
@@ -1,9 +0,0 @@
-package com.sagol.dao;
-
-import com.sagol.dto.userVO;
-
-public interface loginDao {
-		
-	public userVO login(userVO uservo);
-
-}
--- sagol/src/main/java/com/sagol/svc/loginSvc.java
+++
@@ -1,9 +0,0 @@
-package com.sagol.svc;
-
-import com.sagol.dto.userVO;
-
-public interface  loginSvc {
-	
-	public userVO login(userVO uservo);
-
-}
